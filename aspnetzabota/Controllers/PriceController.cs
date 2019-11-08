63d92a63606a7af86b6c4ea3f3b8618fddecc330
mrfrone <mrfrannis@yandex.ru>
2019-11-08 16:16:03 +0300

    add search box

--- aspnetzabota/Controllers/PriceController.cs
+++ aspnetzabota/Controllers/PriceController.cs
@@ -23,14 +23,42 @@ namespace aspnetzabota.Controllers
             return View(result);
 
         }
+        [HttpGet]
         public ActionResult GetItems(int id)
         {
             var result = new PriceServiceViewModel
             {
-                PriceService = _priceService.All.Where(c => c.grcode == id),
                 PriceServiceDep = _priceService.PriceGroupsAndDepartments.Where(c => c.grcode == id)
             };
             return PartialView(result);
         }
+        [HttpGet]
+        public ActionResult PriceTable(int id)
+        {
+            var result = new PriceServiceViewModel
+            {
+                PriceService = _priceService.All.Where(c => c.grcode == id)
+            };
+            return PartialView(result);
+        }
+        [HttpGet]
+        public ActionResult PriceTableAtDepartment(string id)
+        {
+            var result = new PriceServiceViewModel
+            {
+                PriceService = _priceService.All.Where(c => c.depart_name == id)
+            };
+            return PartialView("PriceTable", result);
+        }
+        [HttpGet]
+        public ActionResult PriceTableAtText(string id)
+        {
+            var result = new PriceServiceViewModel
+            {
+                PriceService = _priceService.All.Where(c => c.depart_name.Contains(id))
+            };
+            return PartialView("PriceTable", result);
+        }
+
     }
 }
