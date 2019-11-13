8cc72ed35e780884843541997b63518d3914425e
mrfrone <mrfrannis@yandex.ru>
2019-11-13 16:18:08 +0300

    add ContactUs page

---
+++ aspnetzabota/Controllers/ContactController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspnetzabota.Controllers
+{
+    public class ContactController : Controller
+    {
+        public ViewResult Us()
+        {
+            return View();
+        }
+    }
+}
