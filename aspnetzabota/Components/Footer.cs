ca537757c79ee7392b04386f50620a7396ca6d3a
mrfrone <mrfrannis@yandex.ru>
2019-12-10 12:32:36 +0300

    add footer component

---
+++ aspnetzabota/Components/Footer.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using aspnetzabota.ViewModels;
+using aspnetzabota.Data.Interfaces;
+
+namespace aspnetzabota.Components
+{
+    public class Footer : ViewComponent
+    {
+        private readonly IDepartment _departments;
+        private readonly INews _news;
+
+        public Footer(IDepartment departments, INews news)
+        {
+            _departments = departments;
+            _news = news;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var result = new FooterViewModel
+            {
+                Departments = _departments.Take,
+                News = _news.Last(2)
+            };
+            return View(result);
+        }
+    }
+}
---
+++ aspnetzabota/ViewModels/ComponentsViewModels/FooterViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using aspnetzabota.Data.Models;
+
+namespace aspnetzabota.ViewModels
+{
+    public class FooterViewModel
+    {
+        public IEnumerable<Department> Departments { get; set; }
+        public IEnumerable<News> News { get; set; }
+    }
+}
