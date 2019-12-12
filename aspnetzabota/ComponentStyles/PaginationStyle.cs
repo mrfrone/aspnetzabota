a35b9178279a9bc9468eadc69a15073e58145eeb
mrfrone <mrfrannis@yandex.ru>
2019-12-12 15:24:53 +0300

    add animations

--- aspnetzabota/ComponentStyles/PaginationStyle.cs
+++ aspnetzabota/ComponentStyles/PaginationStyle.cs
@@ -9,7 +9,7 @@ namespace aspnetzabota.ComponentStyles
         public static PagedListRenderOptionsBase PagedListOptions => new PagedListRenderOptionsBase() {
                 LiElementClasses = new List<string> { "page-item" },
                 PageClasses = new string[] { "page-link" },
-                UlElementClasses = new List<string> { "pagination" },
+                UlElementClasses = new List<string> { "pagination", "ftco-animate" },
                 ContainerDivClasses = new List<string> { "nav text-xs-center", "justify-content-center" },
                 LinkToPreviousPageFormat = "Назад",
                 LinkToNextPageFormat = "Вперед",
