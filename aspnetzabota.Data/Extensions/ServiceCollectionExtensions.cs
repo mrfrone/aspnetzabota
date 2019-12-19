dd2eb421b72dc9ae0fe970c18738451d57a10d88
mrfrone <mrfrannis@yandex.ru>
2019-12-19 15:58:58 +0300

    Encapsulated ContentServices

---
+++ aspnetzabota.Data/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using aspnetzabota.Content.Database.Context;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+namespace aspnetzabota.Content.Database.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddContentDatabase(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<ContentContext>(options =>
+                options.UseMySql(connectionString, opt =>
+                           opt.MigrationsHistoryTable("__EFMigrationsHistory")));
+
+            return services;
+        }
+    }
+}
--- aspnetzabota/Startup.cs
+++ aspnetzabota/Startup.cs
@@ -3,23 +3,14 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json.Serialization;
 using Newtonsoft.Json;
-using aspnetzabota.Content.Database.Context;
-using aspnetzabota.Content.Database.Repository.News;
-using aspnetzabota.Content.Database.Repository.Category;
-using aspnetzabota.Content.Database.Repository.Department;
-using aspnetzabota.Content.Database.Repository.Licenses;
-using aspnetzabota.Content.Database.Repository.Review;
-using aspnetzabota.Content.Database.Repository.Slider;
-using aspnetzabota.Content.Services.Price;
-using aspnetzabota.Content.Services.Schedule;
-using Microsoft.EntityFrameworkCore;
+using aspnetzabota.Content.Services.Extensions;
 using Microsoft.Extensions.Configuration;
 
 namespace aspnetzabota
 {
     public class Startup
     {
-        private IConfigurationRoot _config;
+        private readonly IConfigurationRoot _config;
 
         public Startup(IHostingEnvironment hostEnv)
         {
@@ -31,18 +22,9 @@ namespace aspnetzabota
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ContentContext>(options => 
-                options.UseMySQL(_config.GetConnectionString("DefaultConnection"), opt =>
-                           opt.MigrationsHistoryTable("__EFMigrationsHistory")));
+            var connectionString = _config.GetConnectionString("DefaultConnection");
+            services.AddContentRepository(connectionString);
 
-            services.AddScoped<ILicenses, LicensesRepository>();
-            services.AddScoped<IDepartment, DepartmentRepository>();
-            services.AddScoped<IPrice, Price>();
-            services.AddScoped<ISchedule, Schedule>();
-            services.AddScoped<IReview, ReviewRepository>();
-            services.AddScoped<ISlider, SliderRepository>();
-            services.AddScoped<INews, NewsRepository>();
-            services.AddScoped<ICategory, CategoryRepository>();
             services.AddMvcCore().AddJsonFormatters().AddJsonOptions(options =>
             {
                 options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
