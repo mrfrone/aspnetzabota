703e8cc4b2bf26777472ee2a576798e769328307
mrfrone <mrfrannis@yandex.ru>
2019-12-19 11:54:52 +0300

    Revert "fix index page load"
    This reverts commit 72f77e55540b6ef8dcbcba0f84f796c33f9aba30.

--- aspnetzabota.Content.Database.Repository/Review/IReview.cs
+++ aspnetzabota.Content.Database.Repository/Review/IReview.cs
@@ -6,7 +6,7 @@ namespace aspnetzabota.Content.Database.Repository.Review
     public interface IReview
     {
         IEnumerable<Entities.Review> Take { get; }
-        IEnumerable<Entities.Review> Last(int Count);
+        IEnumerable<Entities.Review> Random(int Count);
         IEnumerable<Entities.Review> GetPagedList(int pageNumber, int pageSize);
         Task Add(Entities.Review review);
     }
