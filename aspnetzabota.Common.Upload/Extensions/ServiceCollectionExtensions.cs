ff498594859696ffa8c43c5879c6d2d65aa9aadc
frone <mrfronne@gmail.com>
2020-04-27 13:58:20 +0300

    end images system

--- aspnetzabota.Common.Upload/Extensions/ServiceCollectionExtensions.cs
+++ aspnetzabota.Common.Upload/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,4 @@
-﻿using aspnetzabota.Common.Upload;
+﻿using aspnetzabota.Common.Upload.UploadPath;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace aspnetzabota.Common.Upload.Extensions
@@ -8,6 +8,7 @@ namespace aspnetzabota.Common.Upload.Extensions
         public static IServiceCollection AddUpload(this IServiceCollection services)
         {
             services.AddScoped<IUpload, Upload>();
+            services.AddScoped<IUploadPath, UploadPath.UploadPath>();
 
             return services;
         }
